d73be55792978a02efa0873852a6078138867318
Nathan Van Gheem <vangheem@gmail.com>
2019-01-03 18:57:02 -0500

    implement exists for head requests (#6)
    * implement exists for head requests
    
    * fix check
    
    * require latest guillotina

--- guillotina_s3storage/tests/fixtures.py
+++ guillotina_s3storage/tests/fixtures.py
@@ -11,22 +11,18 @@ def settings_configurator(settings):
     else:
         settings['applications'] = ['guillotina_s3storage']
 
-    settings.update({
-        'utilities': [{
-            "provides": "guillotina_s3storage.interfaces.IS3BlobStore",
-            "factory": "guillotina_s3storage.storage.S3BlobStore",
-            "settings": {
-                "bucket": os.environ.get('S3CLOUD_BUCKET', 'testbucket'),
-                "aws_client_id": os.environ.get('S3CLOUD_ID', 'xxx'),
-                "aws_client_secret": os.environ.get('S3CLOUD_SECRET', 'xxx')  # noqa
-            }
-        }],
-        'static': {},
-        'jsapps': {}
-    })
+    settings['load_utilities']['s3'] = {
+        "provides": "guillotina_s3storage.interfaces.IS3BlobStore",
+        "factory": "guillotina_s3storage.storage.S3BlobStore",
+        "settings": {
+            "bucket": os.environ.get('S3CLOUD_BUCKET', 'testbucket'),
+            "aws_client_id": os.environ.get('S3CLOUD_ID', 'xxx'),
+            "aws_client_secret": os.environ.get('S3CLOUD_SECRET', 'xxx')  # noqa
+        }
+    }
 
     if 'S3CLOUD_ID' not in os.environ:
-        settings['utilities'][0]['settings'].update({
+        settings['load_utilities']['s3']['settings'].update({
             'endpoint_url': 'http://localhost:5000',
             'verify_ssl': False,
             'ssl': False,
